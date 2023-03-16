44a0d1f60e0282a849261a02d186cc6ac85edcf4
Samikizilboga <samikizilboga@finesci.com>
2023-03-17 00:13:54 +0300

    profile_screen

--- lib/components/profile_screen/profile.dart
+++ lib/components/profile_screen/profile.dart
@@ -1,36 +1,79 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:instagram_clone/components/profile_screen/user_profile_followers.dart';
 
 import '../stories/horizontal_listview.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
-  
+
   get icon_path => null;
-  
+
   get icon_myself => null;
-  
+
   get icon_name => null;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: SafeArea(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Container(
-                  child: Horizontal_Listview(
-                    icon_path: 'assets/icons/beer.png',
-                    icon_name: 'Beer',
-                    size: 0.1,
+        child: Padding(
+          padding: EdgeInsets.only(left: 20, right: 40, top: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    child: Horizontal_Listview(
+                      icon_path: 'assets/icons/beer.png',
+                      icon_name: 'Beer',
+                      size: 0.1,
+                    ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                  Followers(
+                    num: 250,
+                    text: 'Posts',
+                  ),
+                  Followers(
+                    num: 1500,
+                    text: 'Followers',
+                  ),
+                  Followers(
+                    num: 165,
+                    text: 'Following',
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Username",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15),
+              ),
+              Text(
+                "Username",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15),
+              ),
+              Text(
+                "Username",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15),
+              ),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/components/profile_screen/user_profile_followers.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Followers extends StatelessWidget {
+  final int? num;
+  final String? text;
+  const Followers({super.key, this.text, this.num});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          num.toString(),
+          style: TextStyle(color: Colors.white, fontSize: 20),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+          text.toString(),
+          style: TextStyle(color: Colors.white, fontSize: 12),
+        )
+      ],
+    );
+  }
+}
